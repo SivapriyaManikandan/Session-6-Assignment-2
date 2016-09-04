6e4ba1310ea813c7b36c0ad9fbdf8276eb30590c
SivapriyaManikandan <way2priyamail@gmail.com>
2016-09-04 20:52:38 +0530

    Add files via upload

---
+++ largest.js
@@ -0,0 +1,15 @@
+var a = 10;
+var b = 20;
+var c = 15;
+if (a>b && a>c)
+    { 
+    console.log(a);
+    }
+if (b>a && b>c)
+    {
+        console.log(b);
+        }
+else{
+    console.log(c);
+}
+alert("largernumber");
